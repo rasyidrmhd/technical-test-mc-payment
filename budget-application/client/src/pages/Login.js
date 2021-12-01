60c2743a667c6b9842a45d54bff06f1fd8caef73
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-12-02 02:04:37 +0700

    login done with poor UI

--- budget-application/client/src/pages/Login.js
+++ budget-application/client/src/pages/Login.js
@@ -1,20 +1,61 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory, useLocation } from "react-router";
+import { login } from "../store/actions/userAction";
 
 export default function Login() {
   const location = useLocation();
-  console.log(location);
+  const dispatch = useDispatch();
   const history = useHistory();
-  const { userdata } = useSelector((state) => state.userReducer);
+  const [inputLogin, setInputLogin] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState();
+
+  const handleInputLogin = (e) => {
+    const { name, value } = e.target;
+
+    setInputLogin({
+      ...inputLogin,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(inputLogin))
+      .then((response) => {
+        localStorage.setItem("access_token", response);
+        history.push("/");
+      })
+      .catch((err) => {
+        setErrors(err);
+      });
+  };
 
   return (
     <>
       <h1>Hello from Login</h1>
       {location.state ? <span>{location.state.message}</span> : ""}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Username / Email</label>
         <br />
+        <input type="text" placeholder="Enter you username or email" autoComplete="off" name="email" defaultValue={inputLogin.email} onChange={handleInputLogin} /> <br />
+        <label>Password</label>
+        <br />
+        <input type="password" placeholder="Enter your strong password" autoComplete="off" name="password" defaultValue={inputLogin.password} onChange={handleInputLogin} />
+        <br />
+        <button type="submit">Login</button>
       </form>
+      {errors ? (
+        <>
+          <span>{errors}</span>
+          <br />
+        </>
+      ) : (
+        ""
+      )}
       <button
         onClick={(e) => {
           e.preventDefault();

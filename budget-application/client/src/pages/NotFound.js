950bd31b372274246cbc2570e948a01560a06759
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-12-02 20:05:45 +0700

    add page not found

--- budget-application/client/src/pages/NotFound.js
+++ budget-application/client/src/pages/NotFound.js
@@ -1,7 +1,27 @@
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSadTear } from "@fortawesome/free-solid-svg-icons";
+
 export default function NotFound() {
+  const history = useHistory();
+
   return (
-    <>
-      <h1>Page not found</h1>
-    </>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+      <div className="text-center">
+        <FontAwesomeIcon icon={faSadTear} size="7x" />
+        <h2>
+          Sorry, the page you are looking for <br /> is not found
+        </h2>
+        <button
+          className="btn btn-primary rounded-pill"
+          onClick={(e) => {
+            e.preventDefault();
+            history.goBack();
+          }}
+        >
+          Back
+        </button>
+      </div>
+    </div>
   );
 }
